689e6bd803685da587cccce30f3de06d94b129ae
LawrenceYamete <LawrenceYamete>
2023-07-16 04:38:19 +0800

    updates

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: 'Task Master API',
     description: 'Stores tasks, users, teams and inventory info and allows users to CRUD.',
   },
-  host: 'taskmaster-project.onrender.com',
-  schemes: ['https', 'http']
+  host: 'taskmaster-oq75.onrender.com',
+  schemes: ['https'],
 };
 
 const outputFile = './swagger-output.json';
